e0b1e402905cec4dcd90e9bf215be08d375602f9
mac <kim@useok.local>
2025-10-27 13:50:56 +0900

    첫번째 게임로직완성

--- src/main/java/racingcar/Gameprocess.java
+++ src/main/java/racingcar/Gameprocess.java
@@ -54,7 +54,6 @@ public class Gameprocess {
         carNames[carName] = stringBuilderb.toString();
     }
 
-
     //게임횟수셋팅 메서드
     void gameCountSetting() {
         System.out.print("시도할 횟수는 몇 회인가요?  :");
